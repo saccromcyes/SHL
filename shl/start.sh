2cf00dc049f50a0806ef5c61c7d5fd8cadb90c04
Aakarsh Dutt Sharma <aakarshduttsharma123@gmail.com>
2025-05-05 22:56:38 +0530

    Update start.sh

--- shl/start.sh
+++ shl/start.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Navigate to the directory where your Flask app is located (optional, if needed)
-# cd path/to/your/app
-
-
 # Start the Flask app
-export FLASK_APP="C:/Users/ndsha/Desktop/shl/api/main.py" # update this path to your actual main Flask file
+export FLASK_APP=main
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=$PORT
 
